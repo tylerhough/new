bad87c09e6276b2402fa485eaba59d234338ad15
tylerhough <33042307+tylerhough@users.noreply.github.com>
2020-02-15 18:33:50 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var http = require('http');
 http.createServer(function (req, res) {
   //res.write('Hello World!'); //write a response to the client
   //res.end(); //end the response
-  res.send('<html>'
-              +'<head></head>'
-              +'<body><center></center></body>'
+  res.send("<html>"
+              +"<head></head>"
+              +"<body><center><img src='basketball.jpg'></center></body>"
            +'</html>')
 }).listen(8080); //the server object listens on port 8080
